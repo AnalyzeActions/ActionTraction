f1e674dff619c331b2ed5d9d50af539413919b4c
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2021-05-27 14:33:12 -0400

    Create program to download list of GitHub repositories.

---
+++ action-traction/action_traction/traction/download_repos.py
@@ -0,0 +1,13 @@
+from typing import List 
+from pathlib import Path
+import pathlib
+import os
+import git
+
+def download_https(repository_list: List, save_path: Path):
+    # Clone a remote repository using https
+    for repo in repository_list:
+        repo_name = os.path.splitext(os.path.basename(repo))[0]
+        # print(repo_name)
+        path = pathlib.Path.home() / save_path / repo_name
+        git.Repo.clone_from(repo, path)
