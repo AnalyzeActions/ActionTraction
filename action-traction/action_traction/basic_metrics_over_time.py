d770296e0f9c6929210b3810639293ba20c5452e
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2021-07-11 20:37:02 -0400

    Remove unnecessary code segments for collaborators over time.

--- action-traction/action_traction/basic_metrics_over_time.py
+++ action-traction/action_traction/basic_metrics_over_time.py
@@ -32,46 +32,6 @@ def size_over_time(initial_data, repository_set):
     return size_data
 
 
-def authors_over_time(initial_data, repository_set):
-    """Determine the authors of a Actions file over time and generate graph."""
-    dataframe_list = []
-    author_data = pd.DataFrame()
-
-    # Iterate through every repo and generate a unique dataframe for each
-    for repo in repository_set:
-        new_data = initial_data.loc[initial_data['Repository'] == repo]
-        # Remove unnecessary columns from dataframe
-        new_data.drop(["File Size in Bytes", "Committer", "Branches", "Commit Message", "Lines Added", "Lines Removed", "Date of Change"], axis=1)
-        #TODO: Number of authors at one time
-        dataframe_list.append(new_data)
-
-    # Create comprehensive dataframe with authors over every commit
-    for dataframe in dataframe_list:
-        author_data = author_data.append(dataframe)
-    
-    return author_data
-
-
-def committers_over_time(initial_data, repository_set):
-    """Determine the committers of a Actions file over time and generate graph."""
-    dataframe_list = []
-    committer_data = pd.DataFrame()
-
-    # Iterate through every repo and generate a unique dataframe for each
-    for repo in repository_set:
-        new_data = initial_data.loc[initial_data['Repository'] == repo]
-        # Remove unnecessary columns from dataframe
-        new_data.drop(["File Size in Bytes", "Author", "Branches", "Commit Message", "Lines Added", "Lines Removed", "Date of Change"], axis=1)
-        #TODO: Number of committers at one time
-        dataframe_list.append(new_data)
-
-    # Create comprehensive dataframe with committers over every commit
-    for dataframe in dataframe_list:
-        committer_data = committer_data.append(dataframe)
-    
-    return committer_data
-
-
 def lines_added_over_time(initial_data, repository_set):
     """Determine the lines added to a Actions file over time and generate graph."""
     dataframe_list = []
